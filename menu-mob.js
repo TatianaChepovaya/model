af26bb0f165bc5d3f1250a6fa828a74962db36a1
Anna Inozemets <inoznetta@gmail.com>
2023-05-31 15:02:29 +0300

    Fixed bugs for main page

--- menu-mob.js
+++ menu-mob.js
@@ -1,7 +1,35 @@
+function toggleTabContent () {
+  if ($(window).width() < 1023) {
+    $(".objective__tab-content-part:first").show();
+    $(".objective__tab-content-part:not(:first)").hide();
+    $(".tab__button:first").addClass("active");
+    $(".tab__button:not(:first)").removeClass("active");
+    $(".tab__button").click(function() {
+      $(".tab__button").removeClass("active");
+      const tabId = $(this).data("tab");
+      $(".objective__tab-content-part").hide();
+      $("#" + tabId).show();
+      $(this).addClass("active");
+    });
+  } else {
+    $(".objective__tab-content-part").show();
+    $(".tab__button").click(function() {
+      $(".objective__tab-content-part").show();
+      $(".tab__button").removeClass("active");
+    });
+  }
+}
+
 $(document).ready(function () {
-  //   console.log("ready!");
+  toggleTabContent();
+
   $(".menu__mobile-icon").on("click", function () {
-    //  alert();
     $(".header__menu-mobile-container").toggleClass("active");
   });
+
+  $(window).on('resize', function() {
+    toggleTabContent();
+  });
 });
+
+
